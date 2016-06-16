d2d98167f2f190dc570d7b461b6f0551b7e9a9b7
Dominic Ginger <domginger@hotmail.co.uk>
2016-06-16 21:18:19 +0100

    Problem 4 solved

---
+++ p4.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func reverse(str string) string {
+	length := len(str)
+	runes := make([]rune, length)
+	for _, rune := range str {
+		length--
+		runes[length] = rune
+	}
+	return string(runes)
+}
+
+func isPalindrone(number int) bool {
+	str := strconv.Itoa(number)
+	if len(str)%2 != 0 {
+		return false
+	}
+	midway := len(str) / 2
+	first := str[0:midway]
+	second := reverse(str[midway:len(str)])
+
+	return first == second
+}
+
+func biggestPalindroneFactor(start, end int) int {
+	biggest := 0
+	for i := end; i >= start; i-- {
+		for j := end; j >= start; j-- {
+			factor := i * j
+			if isPalindrone(factor) && factor > biggest {
+				biggest = factor
+			}
+		}
+	}
+	return biggest
+}
+
+func main() {
+	biggest := biggestPalindroneFactor(100, 999)
+	fmt.Println(biggest)
+}
